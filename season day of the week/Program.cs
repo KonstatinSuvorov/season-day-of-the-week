dfc94c18854f6993b1451eb03b60448ef494726a
Константин Суворов <kniaze84@outlook.com>
2022-11-07 11:37:56 +0300

    Добавьте файлы проекта.

---
+++ season day of the week/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace season_day_of_the_week
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите год: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите месяц: ");
+            int month = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите день: ");
+            int day = Convert.ToInt32(Console.ReadLine());
+            DateTime dt = new DateTime(year, month, day);
+
+            if (month == 12 || month == 1 || month == 2)
+            {
+                Console.WriteLine("Зима, ");
+            }
+            if (month == 3 || month == 4 || month == 5)
+            {
+                Console.WriteLine("Весна, ");
+            }
+            if (month == 6 || month == 7 || month == 8)
+            {
+                Console.WriteLine("Лето, ");
+            }
+            else
+            {
+                Console.Write("Осень, ");
+            }
+            Console.WriteLine(dt.ToString("dddd"));
+            Console.ReadLine();
+        }
+    }
+}
